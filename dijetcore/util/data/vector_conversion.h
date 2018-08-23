792f58b2bcdacb6a0f16e9aed393f26e83dea275
nickelsey <nicholas.elsey@gmail.com>
2018-08-23 16:10:10 -0400

    removing unused pythia example

--- dijetcore/util/data/vector_conversion.h
+++ dijetcore/util/data/vector_conversion.h
@@ -5,8 +5,6 @@
 // between TLorentzVector, fastjet::PseudoJet,
 // etc
 
-#include <random>
-
 #include "fastjet/PseudoJet.hh"
 #include "TStarJetVectorContainer.h"
 #include "TStarJetVector.h"
@@ -24,33 +22,15 @@ namespace dijetcore {
     
     // Transform TStarJetVectors into (FastJet) PseudoJets
     // ---------------------------------------------------
-    TStarJetVector* sv;
     for(int i = 0; i < container->GetEntries() ; ++i) {
-      sv = container->Get(i);
+      TStarJetVector* sv = container->Get(i);
       
       fastjet::PseudoJet tmpPJ = fastjet::PseudoJet(*sv);
       tmpPJ.set_user_index(sv->GetCharge());
       particles[offset + i] = tmpPJ;
     }
-  }
-  
-  void ConvertPythiaToPseudoJet(const Pythia8::Pythia& gen, std::vector<fastjet::PseudoJet>& particles) {
-    
-    size_t offset = particles.size();
-    size_t final_state = 0;
-    particles.resize(offset + gen.event.size());
-    
-    for (size_t i = 0; i < gen.event.size(); ++i) {
-      if ( gen.event[i].isFinal() && gen.event[i].isVisible() ) {
-        fastjet::PseudoJet tmp(gen.event[i].px(), gen.event[i].py(), gen.event[i].pz(), gen.event[i].e());
-        tmp.set_user_index(gen.event[i].charge());
-        particles[offset + final_state] = tmp;
-        ++final_state;
-      }
-    }
-    particles.resize(offset + final_state);
-  }
-  
+  } 
+ 
 } // dijetcore
 
 #endif // DIJETCORE_UTIL_DATA_VECTOR_CONVERSION_H
