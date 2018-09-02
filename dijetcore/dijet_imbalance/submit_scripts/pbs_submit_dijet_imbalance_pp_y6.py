40cca26745552276b5cb930b7353c43f57907334
nickelsey <nicholas.elsey@gmail.com>
2018-09-01 22:27:50 -0400

    changing binary name in submit script

--- dijetcore/dijet_imbalance/submit_scripts/pbs_submit_dijet_imbalance_pp_y6.py
+++ dijetcore/dijet_imbalance/submit_scripts/pbs_submit_dijet_imbalance_pp_y6.py
@@ -111,7 +111,7 @@ def main(args) :
   
   ## some paths
   execpath = os.getcwd()
-  executable = './bin/dijet_imbalance/dijet_imbalance_pp_y7'
+  executable = './bin/differential_aj_pp_y6_y7_embed'
   ## find the qwrap file
   qwrap = execpath + '/submit/qwrap.sh'
   
