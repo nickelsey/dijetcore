8d2da4dded8c14dc6b03511fbde9eb66cf70329b
nickelsey <nicholas.elsey@gmail.com>
2018-09-13 00:00:56 -0400

    updating typo...

--- dijetcore/dijet_imbalance/submit_scripts/pbs_submit_dijet_imbalance_pp_y6_embed_y14.py
+++ dijetcore/dijet_imbalance/submit_scripts/pbs_submit_dijet_imbalance_pp_y6_embed_y14.py
@@ -111,7 +111,7 @@ def main(args) :
 
   ## some paths
   execpath = os.getcwd()
-  executable = './bin/differential_aj_pp_y6_y7_embed'
+  executable = './bin/differential_aj_pp_y6_y14_embed'
   ## find the qwrap file
   qwrap = execpath + '/submit/qwrap.sh'
 
