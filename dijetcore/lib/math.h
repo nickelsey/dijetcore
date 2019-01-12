271503893cd87356f852144d0843336ac7964e51
Nick Elsey <nicholas.elsey@gmail.com>
2019-01-11 21:27:36 -0500

    adding math header to library

---
+++ dijetcore/lib/math.h
@@ -0,0 +1,10 @@
+#ifndef DIJETCORE_LIB_MATH_H
+#define DIJETCORE_LIB_MATH_H
+
+namespace dijetcore {
+  namespace math {
+    const double pi{ 3.141592653589793238462643383279502884L};
+  }
+}
+
+#endif // DIJETCORE_LIB_MATH_H
