17580472ba053e0e8875711a801010f2922c025b
Nick Elsey <nicholas.elsey@gmail.com>
2019-10-15 13:28:58 -0400

    add default file

--- dijetcore/util/data/efficiency/run14_eff.h
+++ dijetcore/util/data/efficiency/run14_eff.h
@@ -1,67 +1,63 @@
 #ifndef DIJETCORE_UTIL_DATA_EFFICIENCY_RUN14_EFF_H
 #define DIJETCORE_UTIL_DATA_EFFICIENCY_RUN14_EFF_H
 
-#include <vector>
-#include <string>
 #include <memory>
+#include <string>
+#include <vector>
 
-#include "TH1.h"
-#include "TH2.h"
 #include "TF2.h"
 #include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
 
 namespace dijetcore {
-  
-  enum class TrackingUnc {
-    NONE = 0,
-    POSITIVE = 1,
-    NEGATIVE = -1
-  };
-  
-  class Run14Eff {
-    public:
-    
-    Run14Eff(std::string filename = "", int nBinsZDC = 10, int nBinsCent = 16);
-    
-    ~Run14Eff();
-    
-    void loadFile(std::string filename, int nBinsZDC = 10, int nBinsCent = 16);
-    
-    double AuAuEff(double pt, double eta, int cent, double zdcrate);
-    double pp6Eff(double pt, double eta);
-    double ratio(double pt, double eta, int cent, double zdcrate);
-    double ratioUncertainty(double pt, double eta, int cent, double zdcrate);
-    
-    int luminosityBin(double zdcrate);
-    
-    void setSystematicUncertainty(TrackingUnc sys = TrackingUnc::NONE) {sys_ = sys;}
-    TrackingUnc SystematicUncertainty() const {return sys_;}
-    
-    void setAuAuUncertainty(double unc) {auau_u_ = unc;}
-    double AuAuUncertainty() const {return auau_u_;}
-    void setPPUncertainty(double unc) {pp_u_ = unc;}
-    double PPUncertainty() const {return pp_u_;}
-    void setCentUncertainty(double unc) {cent_u_ = unc;}
-    double CentUncertainty() const {return cent_u_;}
-    
-    private:
-    
-    void loadCurves(int nBinsZDC =10, int nBinsCent = 16);
-    
-    std::shared_ptr<TFile> file;
-    
-    std::vector<std::vector<TH2D*>> curves;
-    std::shared_ptr<TF2> effY06; // Run 6 parameterization
-    
-    double max_pt_;
-    
-    TrackingUnc sys_;
-    double auau_u_;
-    double pp_u_;
-    double cent_u_;
-    
-  };
-  
+
+enum class TrackingUnc { NONE = 0, POSITIVE = 1, NEGATIVE = -1 };
+
+class Run14Eff {
+public:
+  Run14Eff(std::string filename = "resources/efficiencies/y14_p18ih_dca3.root",
+           int nBinsZDC = 10, int nBinsCent = 16);
+
+  ~Run14Eff();
+
+  void loadFile(std::string filename, int nBinsZDC = 10, int nBinsCent = 16);
+
+  double AuAuEff(double pt, double eta, int cent, double zdcrate);
+  double pp6Eff(double pt, double eta);
+  double ratio(double pt, double eta, int cent, double zdcrate);
+  double ratioUncertainty(double pt, double eta, int cent, double zdcrate);
+
+  int luminosityBin(double zdcrate);
+
+  void setSystematicUncertainty(TrackingUnc sys = TrackingUnc::NONE) {
+    sys_ = sys;
+  }
+  TrackingUnc SystematicUncertainty() const { return sys_; }
+
+  void setAuAuUncertainty(double unc) { auau_u_ = unc; }
+  double AuAuUncertainty() const { return auau_u_; }
+  void setPPUncertainty(double unc) { pp_u_ = unc; }
+  double PPUncertainty() const { return pp_u_; }
+  void setCentUncertainty(double unc) { cent_u_ = unc; }
+  double CentUncertainty() const { return cent_u_; }
+
+private:
+  void loadCurves(int nBinsZDC = 10, int nBinsCent = 16);
+
+  std::shared_ptr<TFile> file;
+
+  std::vector<std::vector<TH2D *>> curves;
+  std::shared_ptr<TF2> effY06; // Run 6 parameterization
+
+  double max_pt_;
+
+  TrackingUnc sys_;
+  double auau_u_;
+  double pp_u_;
+  double cent_u_;
+};
+
 } // namespace dijetcore
 
 #endif // DIJETCORE_UTIL_DATA_EFFICIENCY_RUN14_EFF_H
