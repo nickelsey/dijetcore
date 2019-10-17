19f41ddef28371c3a2ce49795941361f0e13153c
Nick Elsey <nicholas.elsey@gmail.com>
2019-10-17 11:41:20 -0400

    adding debug info

--- dijetcore/dijet_imbalance/differential_aj_pp_y6_y14_embed_pico.cc
+++ dijetcore/dijet_imbalance/differential_aj_pp_y6_y14_embed_pico.cc
@@ -582,9 +582,11 @@ int main(int argc, char *argv[]) {
       // we're using this p+p event - find the trigger object then start the embedding loop
       std::vector<fastjet::PseudoJet> triggers;
       std::vector<int> trigger_tow_ids;
-      LOG(INFO) << "trig objs: " << reader->GetEvent()->GetHeader()->GetNOfTrigObjs();
       for (int i = 0; i < reader->GetEvent()->GetHeader()->GetNOfTrigObjs(); ++i) {
         TStarJetPicoTriggerInfo* t = reader->GetEvent()->GetTrigObj(i);
+        LOG(INFO) << "trigger object: " << i;
+        t->PrintInfo();
+        LOG(INFO) << "trigger flag: " << t->GetTriggerFlag();
         if (t->isBHT2() || t->isBHT3()) {
           int idx = t->GetId();
           double eta = t->GetEta();
