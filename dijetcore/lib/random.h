d04736f8546df600b4cbf07d9735662f57a9218a
Nick Elsey <nicholas.elsey@gmail.com>
2020-02-22 23:08:46 -0500

    add random methods from sct for convenience

---
+++ dijetcore/lib/random.h
@@ -0,0 +1,74 @@
+#ifndef DIJETCORE_LIB_RANDOM_H
+#define DIJETCORE_LIB_RANDOM_H
+
+// implements commonly used random number distributions
+
+// to use the counter inside the dijetcore library, call the singleton
+// Counter::instance().
+
+// the random number generator is thread_local, so each thread should set its
+// own globally unique seed, or let a random seed be used
+
+#include <atomic>
+#include <mutex>
+#include <random>
+
+namespace dijetcore {
+
+class Counter {
+public:
+  static Counter &instance();
+  virtual ~Counter();
+
+  // returns a globally unique counter, useful for histogram naming where
+  // identical names can cause problems... thanks ROOT
+  inline unsigned counter() { return counter_++; }
+
+private:
+  std::atomic<unsigned> counter_;
+
+  Counter();
+  Counter(const Counter &);
+};
+
+class Random {
+public:
+  static Random &instance();
+  virtual ~Random();
+
+  // samples [0, 1] uniformly
+  double uniform();
+
+  // samples [0, 2] uniformly
+  double uniform2();
+
+  // samples [-1, 1] uniformly
+  double centeredUniform();
+
+  // samples [0, pi] uniformly
+  double zeroToPi();
+
+  // samples [0, 1] with a linearly increasing probability distribution
+  // proportional to x
+  double linear();
+
+  // reset the seed for the random number engine - a value less than zero will
+  // make a call to std::random_device, for a random seed.
+  void seed(int seed = -1);
+
+private:
+  std::mt19937 generator_;
+
+  std::uniform_real_distribution<> unit_uniform_;
+  std::uniform_real_distribution<> two_unit_uniform_;
+  std::uniform_real_distribution<> two_unit_centered_uniform_;
+  std::uniform_real_distribution<> zero_to_pi_;
+
+  std::piecewise_linear_distribution<> unit_linear_;
+
+  Random();
+  Random(const Random &);
+};
+} // namespace dijetcore
+
+#endif // DIJETCORE_LIB_RANDOM_H
---
+++ dijetcore/lib/random_benchmark.cc
@@ -0,0 +1,43 @@
+#include "dijetcore/lib/logging.h"
+#include "dijetcore/lib/random.h"
+
+#include "benchmark/benchmark.h"
+
+#include "TF1.h"
+#include "TH1.h"
+
+static void BM_random_sample(benchmark::State& state) {
+  dijetcore::Random& rand = dijetcore::Random::instance();
+  double total = 0.0;
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(total += rand.linear());
+  }
+}
+
+static void BM_root_th1_sample(benchmark::State& state) {
+  TH1D* h = new TH1D("h", "h", 100, 0, 100);
+  for (int i = 1; i <= 100; ++i) h->SetBinContent(i, i);
+
+  double total = 0.0;
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(total += h->GetRandom());
+  }
+  delete h;
+}
+
+static void BM_root_tf1_sample(benchmark::State& state) {
+  TF1* f = new TF1("f", "[0]*x", 0, 100);
+  f->SetParameter(0, 1);
+
+  double total = 0.0;
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(total += f->GetRandom());
+  }
+  delete f;
+}
+
+BENCHMARK(BM_random_sample)->Range(1e4, 1e6);
+BENCHMARK(BM_root_th1_sample)->Range(1e4, 1e6);
+BENCHMARK(BM_root_tf1_sample)->Range(1e4, 1e6);
+
+BENCHMARK_MAIN();
