91dcd16894480ce5005a3f87f080b8061deaec14
Nick Elsey <nicholas.elsey@gmail.com>
2020-03-22 21:13:05 -0400

    update

--- dijetcore/data/general/qa/basic_embedding_qa.cc
+++ dijetcore/data/general/qa/basic_embedding_qa.cc
@@ -10,21 +10,21 @@
 #include <set>
 #include <string>
 
+#include "TCanvas.h"
 #include "TFile.h"
 #include "TTree.h"
-#include "TCanvas.h"
 
 #include "TStarJetPicoEvent.h"
 #include "TStarJetPicoEventHeader.h"
-#include "TStarJetPicoReader.h"
 #include "TStarJetPicoPrimaryTrack.h"
+#include "TStarJetPicoReader.h"
 
 DIJETCORE_DEFINE_string(
     tree, "", "input root TFile (or txt file with TFile list) with JetTree(s)");
 DIJETCORE_DEFINE_string(towList, "resources/bad_tower_lists/empty_list.txt",
                         "bad tower list, default is empty list");
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   // setup command line flags
   dijetcore::SetUsageMessage("Generate a tree of all unique runids");
   dijetcore::ParseCommandLineFlags(&argc, argv);
@@ -33,42 +33,53 @@ int main(int argc, char* argv[]) {
   dijetcore::InitLogging(argv[0]);
 
   // build our input chain of ROOT tree(s) and init reader
-  TChain* chain = dijetcore::NewChainFromInput(FLAGS_tree);
-  TChain* mc_chain = dijetcore::NewChainFromInput(FLAGS_tree, "JetTreeMc");
+  TChain *chain = dijetcore::NewChainFromInput(FLAGS_tree);
+  TChain *mc_chain = dijetcore::NewChainFromInput(FLAGS_tree, "JetTreeMc");
 
-  TStarJetPicoReader* reader = new TStarJetPicoReader();
-  if (chain != nullptr) reader->SetInputChain(chain);
+  TStarJetPicoReader *reader = new TStarJetPicoReader();
+  if (chain != nullptr)
+    reader->SetInputChain(chain);
+  reader->GetTowerCuts()->AddBadTowers(FLAGS_towList);
   reader->Init(-1);
 
-  TStarJetPicoReader* mc_reader = new TStarJetPicoReader();
-  if (chain != nullptr) mc_reader->SetInputChain(mc_chain);
+  TStarJetPicoReader *mc_reader = new TStarJetPicoReader();
+  if (chain != nullptr)
+    mc_reader->SetInputChain(mc_chain);
+  mc_reader->GetTowerCuts()->AddBadTowers(FLAGS_towList);
   mc_reader->Init(-1);
 
-  TH1D* dca = new TH1D("dca", "", 50, 0, 5);
-  TH1D* mc_dca = new TH1D("mcdca", "", 50, 0, 5);
+  TH1D *dca = new TH1D("dca", "", 50, 0, 5);
+  TH1D *mc_dca = new TH1D("mcdca", "", 50, 0, 5);
 
   for (int i = 0; i < reader->GetNOfEvents(); ++i) {
     reader->ReadEvent(i);
-    mc_reader->ReadEvent(i);
-    TClonesArray* r_array = reader->GetEvent()->GetPrimaryTracks();
-    TClonesArray* mc_array = mc_reader->GetEvent()->GetPrimaryTracks();
-    TIter r_iter(r_array);
-    TIter mc_iter(mc_array);
 
-    while (TStarJetPicoPrimaryTrack* track = (TStarJetPicoPrimaryTrack*) r_iter.Next()) {
+    TClonesArray *r_array = reader->GetEvent()->GetPrimaryTracks();
+
+    TIter r_iter(r_array);
+
+    while (TStarJetPicoPrimaryTrack *track =
+               (TStarJetPicoPrimaryTrack *)r_iter.Next()) {
       dca->Fill(track->GetDCA());
     }
-    while (TStarJetPicoPrimaryTrack* track = (TStarJetPicoPrimaryTrack*) mc_iter.Next()) {
+
+    mc_reader->ReadEvent(i);
+
+    TClonesArray *mc_array = mc_reader->GetEvent()->GetPrimaryTracks();
+
+    TIter mc_iter(mc_array);
+    while (TStarJetPicoPrimaryTrack *track =
+               (TStarJetPicoPrimaryTrack *)mc_iter.Next()) {
       mc_dca->Fill(track->GetDCA());
     }
   }
 
   TCanvas c;
-  dca->Scale(1.0/dca->Integral());
-  mc_dca->Scale(1.0/mc_dca->Integral());
-  
+  dca->Scale(1.0 / dca->Integral());
+  mc_dca->Scale(1.0 / mc_dca->Integral());
+
   dca->Draw();
-  mc_dca->Draw();
+  mc_dca->Draw("SAME");
   c.SaveAs("tmp.pdf");
 
   google::ShutdownGoogleLogging();
