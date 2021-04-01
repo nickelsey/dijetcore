2ea1a83fc9d5c2383a1e115cf070257dea7f1a7e
nickelsey <nicholas.elsey@gmail.com>
2021-03-31 22:32:49 -0400

    add random utilities from master

---
+++ dijetcore/lib/random.cc
@@ -0,0 +1,67 @@
+#include "dijetcore/lib/random.h"
+
+#include "dijetcore/lib/math.h"
+
+namespace dijetcore {
+
+Counter::Counter() : counter_(0) {}
+
+Counter::~Counter() {}
+
+Counter &Counter::instance() {
+  static Counter instance;
+  return instance;
+}
+
+Random::Random()
+    : unit_uniform_(0.0, 1.0), two_unit_uniform_(0.0, 2.0),
+      two_unit_centered_uniform_(-1.0, 1.0), zero_to_pi_(0.0, math::pi) {
+  std::vector<double> x = {0.0, 1.0};
+  std::vector<double> w = {0.0, 1.0};
+  unit_linear_ =
+      std::piecewise_linear_distribution<>(x.begin(), x.end(), w.begin());
+
+  generator_.seed(54854);
+}
+
+Random &Random::instance() {
+  thread_local static Random instance;
+  return instance;
+}
+
+Random::~Random() {}
+
+double Random::uniform() {
+  double ret = unit_uniform_(generator_);
+  return ret;
+}
+
+double Random::uniform2() {
+  double ret = two_unit_uniform_(generator_);
+  return ret;
+}
+
+double Random::centeredUniform() {
+  double ret = two_unit_centered_uniform_(generator_);
+  return ret;
+}
+
+double Random::zeroToPi() {
+  double ret = zero_to_pi_(generator_);
+  return ret;
+}
+
+double Random::linear() {
+  double ret = unit_linear_(generator_);
+  return ret;
+}
+
+void Random::seed(int seed) {
+  if (seed < 0) {
+    std::random_device rd;
+    generator_.seed(rd());
+  } else {
+    generator_.seed(seed);
+  }
+}
+} // namespace dijetcore
---
+++ dijetcore/lib/random_test.cc
@@ -0,0 +1,116 @@
+#include "dijetcore/lib/random.h"
+#include "dijetcore/lib/logging.h"
+
+#include "gtest/gtest.h"
+
+#include <future>
+#include <stdlib.h>
+#include <thread>
+#include <vector>
+
+// tests the boundaries and the averages of the
+// distributions implemented in random.h
+
+TEST(unit_random, bounds) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 1e6;
+  for (int i = 0; i < n_throws; ++i) {
+    double tmp = random.uniform();
+    EXPECT_LE(0.0, tmp);
+    EXPECT_GE(1.0, tmp);
+  }
+}
+
+TEST(unit_random, average) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 1e6;
+  double counter = 0.0;
+  for (int i = 0; i < n_throws; ++i) {
+    counter += random.uniform();
+  }
+  EXPECT_NEAR(0.5, counter / n_throws, 1e-3);
+}
+
+TEST(two_unit_random, bounds) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 1e6;
+  for (int i = 0; i < n_throws; ++i) {
+    double tmp = random.uniform2();
+    EXPECT_LE(0.0, tmp);
+    EXPECT_GE(2.0, tmp);
+  }
+}
+
+TEST(two_unit_random, average) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 5e6;
+  double counter = 0.0;
+  for (int i = 0; i < n_throws; ++i) {
+    counter += random.uniform2();
+  }
+  EXPECT_NEAR(1.0, counter / n_throws, 1e-3);
+}
+
+TEST(two_unit_center_random, bounds) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 1e6;
+  for (int i = 0; i < n_throws; ++i) {
+    double tmp = random.centeredUniform();
+    EXPECT_LE(-1.0, tmp);
+    EXPECT_GE(1.0, tmp);
+  }
+}
+
+TEST(two_unit_center_random, average) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 5e6;
+  double counter = 0.0;
+  for (int i = 0; i < n_throws; ++i) {
+    counter += random.centeredUniform();
+  }
+  EXPECT_NEAR(0.0, counter / n_throws, 1e-3);
+}
+
+TEST(linear_random, bounds) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 1e5;
+  for (int i = 0; i < n_throws; ++i) {
+    double tmp = random.linear();
+    EXPECT_LE(0.0, tmp);
+    EXPECT_GE(1.0, tmp);
+  }
+}
+
+TEST(linear_random, average) {
+  dijetcore::Random &random = dijetcore::Random::instance();
+  unsigned n_throws = 1e6;
+  double counter = 0.0;
+  for (int i = 0; i < n_throws; ++i) {
+    counter += random.linear();
+  }
+  EXPECT_NEAR(2.0 / 3.0, counter / n_throws, 1e-3);
+}
+
+double seed_thread_test(int seed) {
+
+  dijetcore::Random::instance().seed(seed);
+  sleep(4);
+  return dijetcore::Random::instance().uniform();
+}
+
+TEST(random, threaded) {
+  std::vector<double> results;
+  auto future1 = std::async(seed_thread_test, 1);
+  sleep(1);
+  auto future2 = std::async(seed_thread_test, 1);
+  sleep(1);
+  auto future3 = std::async(seed_thread_test, 1);
+  auto future4 = std::async(seed_thread_test, 1323);
+  double result1 = future1.get();
+  double result2 = future2.get();
+  double result3 = future3.get();
+  double result4 = future4.get();
+  EXPECT_EQ(result1, result2);
+  EXPECT_EQ(result1, result3);
+  EXPECT_NE(result3, result4);
+}
